c059f8378ddb03c8245d2215dd9696f9a7716a50
mayankkuhaad <mayankkuhaad@gmail.com>
2022-11-20 02:50:03 +0530

    modified mongoDB uri to atlas

--- connection/connect.js
+++ connection/connect.js
@@ -1,5 +1,6 @@
 const mongoose= require("mongoose");
-
-mongoose.connect('mongodb://localhost:27017/restapi')
+const uri = process.env.MONGODB_URI;
+// mongoose.connect('mongodb://localhost:27017/restapi')
+mongoose.connect(uri)
 .then(console.log("Login successful"))
   .catch(console.error);
